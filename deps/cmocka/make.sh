b16c1319c2169cea35636928c07a6b72a8088a1a
Przemyslaw Dobrowolski <przemyslaw.dobrowolski@mini.pw.edu.pl>
2017-01-06 21:26:54 +0100

    Ugrade project files and deps to support OpenBSD

--- deps/cmocka/make.sh
+++ deps/cmocka/make.sh
@@ -7,21 +7,21 @@ else
 (
     echo "cmocka: build"
     rm -rf include lib build
-    tar xf cmocka-1.1.0.tar.xz
+    tar zxvf cmocka-1.1.0.tar.gz
     mv cmocka-1.1.0 build
     (
         cd build;
         mkdir tmp-build
         (
             cd tmp-build
-            cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`pwd`/../out
+            cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`pwd`/../out -DWITH_STATIC_LIB=ON
             make
             make install
         )
     )
     mv build/out/include .
     mkdir lib
-    cp build/out/lib/libcmocka*.so* lib
+    cp build/out/lib/libcmocka.a lib
     echo "cmocka: done"
 )
 fi
