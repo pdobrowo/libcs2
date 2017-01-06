ab186e86c032a7c2169ab8cc7a76162444c6245d
Przemyslaw Dobrowolski <przemyslaw.dobrowolski@mini.pw.edu.pl>
2017-01-06 21:26:54 +0100

    Ugrade project files and deps to support OpenBSD

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,26 +1,32 @@
 #
-# Try to find the GMP librairies
+# Try to find the GMP libraries
 #
-# GMP_FOUND - system has GMP lib
-# GMP_INCLUDE_DIR - the GMP include directory
-# GMP_LIBRARIES - libraries needed to use GMP
+# GMP_FOUND - GMP found
+# GMP_INCLUDE_DIR - GMP include directory
+# GMP_LIBRARIES - GMP shared libraries
+# GMP_STATIC_LIBRARIES - GMP static libraries
 #
-if (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
+if (GMP_INCLUDE_DIR AND GMP_LIBRARIES AND GMP_STATIC_LIBRARIES)
     set(GMP_FIND_QUIETLY TRUE)
-endif (GMP_INCLUDE_DIR AND GMP_LIBRARIES)
+endif (GMP_INCLUDE_DIR AND GMP_LIBRARIES AND GMP_STATIC_LIBRARIES)
 
 find_path(GMP_INCLUDE_DIR
           NAMES gmp.h
-          PATHS /usr/include/gmp)
+          PATHS /usr/include/gmp
+                /usr/local/include/gmp)
 
 find_library(GMP_LIBRARIES
              NAMES gmp libgmp)
 
+find_library(GMP_STATIC_LIBRARIES
+             NAMES gmp.a libgmp.a)
+
 message(STATUS "GMP_INCLUDE_DIR: ${GMP_INCLUDE_DIR}")
 message(STATUS "GMP_LIBRARIES: ${GMP_LIBRARIES}")
+message(STATUS "GMP_STATIC_LIBRARIES: ${GMP_STATIC_LIBRARIES}")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES)
+find_package_handle_standard_args(GMP DEFAULT_MSG GMP_INCLUDE_DIR GMP_LIBRARIES GMP_STATIC_LIBRARIES)
 
-mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES)
+mark_as_advanced(GMP_INCLUDE_DIR GMP_LIBRARIES GMP_STATIC_LIBRARIES)
 
