1eac384b376919d7fd604136426d92c410ef5a5e
Przemyslaw Dobrowolski <przemyslaw.dobrowolski@mini.pw.edu.pl>
2017-03-29 00:58:50 +0200

    Fixed support for OpenBSD

--- src/mathf.c
+++ src/mathf.c
@@ -34,7 +34,7 @@ void cs2_sincosf(double x, double *s, double *c)
 
 #endif /* defined(__linux__) */
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__)
 
 void cs2_sincosf(double x, double *s, double *c)
 {
@@ -42,4 +42,4 @@ void cs2_sincosf(double x, double *s, double *c)
     *c = cos(x);
 }
 
-#endif /* defined(__FreeBSD__) */
+#endif /* defined(__FreeBSD__) || defined(__OpenBSD__) */
