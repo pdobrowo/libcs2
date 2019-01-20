f2bc3f3b9694e3fa479ccbad22bddccf5fd8fe06
Przemyslaw Dobrowolski <pdobrowo@gmail.com>
2019-01-20 14:36:46 +0100

    Switch cmocka to latest version, use wget

--- deps/cmocka/make.sh
+++ deps/cmocka/make.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 set -e
 
+PKGNAME=cmocka-1.1.3.tar.xz
+
 if [ -d include ]; then
     echo "cmocka: already built"
 else
 (
+    echo "wget cmocka $PKGNAME"
+    wget http://cmocka.org/files/1.1/$PKGNAME
+
     echo "cmocka: build"
     rm -rf include lib build
-    tar zxvf cmocka-1.1.0.tar.gz
-    mv cmocka-1.1.0 build
+    tar xvf $PKGNAME
+    mv `basename $PKGNAME .tar.xz` build
     (
         cd build;
         mkdir tmp-build
@@ -21,7 +26,7 @@ else
     )
     mv build/out/include .
     mkdir lib
-    cp build/out/lib/libcmocka.a lib
+    cp build/out/lib64/libcmocka-static.a lib/libcmocka.a
     echo "cmocka: done"
 )
 fi
